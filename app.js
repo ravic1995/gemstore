b98c42a86b5b7a97ca3e6a35209ffca9df223c85
Ravi <ravi.chandak901@gmail.com>
2014-11-05 22:48:04 +0800

    contoller file

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = angular.module('store',[]);
+(function(){
+    var app = angular.module('store',[]);
+    app.controller('StoreController',function(){
+    this.product = gem;
+    });
+    
+    var gems ={
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: '. . .',
+        canPurchase: true,
+        soldOut: true,
+    }
+    
+})();
